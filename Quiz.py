494321e0c342699f48eab67449784bd9d0dd6aba
ShyamNaidu <syam.svs@gmail.com>
2024-02-02 23:30:23 +0530

    updated Quiz Program

--- Quiz.py
+++ Quiz.py
@@ -4,10 +4,13 @@ response = input("Do you want to answer a question? (Y/N) ")
 
 if response == "y":
     print("Great! Let's start")
-    answer = input("What is the full form of CPU? ")
-    if answer == "Central Process Unit":
-        print("You are correct. Thanks for playing")
-    else:
-        print("Wrong. Try again")
+else:
+    quit()
+
+answer = input("What is the full form of CPU? ")
+if answer == "central processing unit":
+    print("You are correct. Thanks for playing")
+else:
+    print("Wrong. Try again")
 
 
