39a2bee6c6b7251b768663f23cdc97ee8a3a9764
ShyamNaidu <syam.svs@gmail.com>
2024-02-03 05:44:02 +0530

    Update with lower case response

--- Quiz.py
+++ Quiz.py
@@ -2,13 +2,13 @@ print("Welcome to Computer Quiz!!!")
 
 response = input("Do you want to answer a question? (Y/N) ")
 
-if response == "y":
+if response.lower() == "y":
     print("Great! Let's start")
 else:
     quit()
 
 answer = input("What is the full form of CPU? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("You are correct. Thanks for playing")
 else:
     print("Wrong. Try again")
