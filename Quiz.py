10835d1621df58fe6a7cf05f2172fe2eb34fa6b3
ShyamNaidu <syam.svs@gmail.com>
2024-02-03 06:44:57 +0530

    Wrote function to get question and answers

--- Quiz.py
+++ Quiz.py
@@ -1,16 +1,31 @@
-print("Welcome to Computer Quiz!!!")
+qNo = 1
+score = 0
 
-response = input("Do you want to answer a question? (Y/N) ")
+def qBlock(ques,ans):
+    global qNo
+    global score
+    response = input(ques)
+    if response.lower() == ans:
+        print("You are correct. Here is your next question")
+        qNo += 1
+        score += 1
+        return True
+    else:
+        print("You are wrong :(")
+        print(f"You have answered {score} questions with " + str(round((score/qNo) * 100,2)) + "%")
+        return False
+        
+print("Welcome to Computer Quiz!!!")
+response = input("Do you want to answer few question? (Y/N) ")
 
 if response.lower() == "y":
     print("Great! Let's start")
 else:
     quit()
 
-answer = input("What is the full form of CPU? ")
-if answer.lower() == "central processing unit":
-    print("You are correct. Thanks for playing")
-else:
-    print("Wrong. Try again")
+if not qBlock("What is the full form of CPU? ","central processing unit"): quit()
+if not qBlock("What is the full form of RAM? ","random access memory"): quit()
 
+print("Congrats! You have completed the quiz")
+print(f"You have answered {score} questions with " + str(round((score/qNo) * 100,2)) + "%")
 
