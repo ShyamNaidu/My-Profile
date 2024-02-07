9159aef7c39b9f9898c113b82ecb72d113b78ba9
ShyamNaidu <syam.svs@gmail.com>
2024-02-07 02:57:48 +0000

    Created Employee Record Tracker

---
+++ EmployeeRecorder.py
@@ -0,0 +1,15 @@
+def view_emp_records():
+    pass
+
+def add_new_record():
+    pass
+
+
+while true:
+    resp = input("Do you want to view/add new record (V/A/Q for quit): ").LOWER
+    if resp == 'q':
+        break
+    if resp == 'a':
+        add_new_record
+    if resp == 'v'"
+        view_emp_records
