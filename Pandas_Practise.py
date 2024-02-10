787af38882c348ce31135762a66a97412c3855c5
ShyamNaidu <syam.svs@gmail.com>
2024-02-10 14:47:15 +0000

    Pandas Practise

--- Pandas_Practise.py
+++ Pandas_Practise.py
@@ -1,9 +1,63 @@
-import pandas
+import pandas as pd
 
-mydataset = {
-    'family_members' : ["Syam","Urmila","Rushanth","Yoswika"],
-    'ages' : ["40","30","10","5"]
-    }
+## Creating a dataset
+# mydataset = {
+#     'family_member' : ["Syam","Urmila","Rushanth","Yoswika"],
+#     'age' : [40,30,10,5]       
+#     }
 
-myvar = pandas.DataFrame(mydataset)
-print(myvar)
+# myvar = pd.DataFrame(mydataset)
+# print(myvar)
+
+##Prints the version of pandas
+# print(pd.__version__)
+
+##Series - like a column in a Table
+# series_name = ['SNo','Name','Age','Sex']
+# seriesvariable = pd.Series(series_name)
+# print(seriesvariable)
+
+##Working with Labels
+# print("Label Value at index 0 = " + seriesvariable[0])
+# print("Label Value at index 1 = " + seriesvariable[1])
+
+# # Creating own Labels:
+# a = [1,7,2]
+# myseries = pd.Series(a,["x","y","z"])
+# print(myseries)
+# print("Label Value at index 0 = " + str(myseries["x"]))
+
+# # Creating Series with key / value objects
+# person_details = {"SNo":1, "Name":'Syam', "Age":40}
+# person_series = pd.Series(person_details)
+# print(person_series)
+
+# # Creating Series with parts of the Labels
+# person_details = {"SNo":1, "Name":'Syam', "Age":40}
+# person_series = pd.Series(person_details, index = ["SNo","Name"])
+# print(person_series)
+
+# # Creating Data Frames
+# data = {
+#     "SNo" : [1,2,3,4],
+#     "Names" :   ["Syam","Urmila","Rushanth","Yoswika"],
+#     'Ages' : [40,30,10,5]
+# }
+
+# myfamily = pd.DataFrame(data)
+# print(myfamily)
+# print("Printing Row:")
+# print(myfamily.loc[1])  #prints the 2nd record in a DataFrame
+# print("Printing First Two Rows:")
+# print(myfamily.loc[[0,1]])  #prints the first 2 records in a DataFrame
+
+# #Creating own indexes instead of number indexing
+# myfamily = pd.DataFrame(data,index=["Father","Mother","Son","Daughter"])
+# print(myfamily)
+# print("Printing Father Record:")
+# print(myfamily.loc["Father"])  #prints the 2nd record in a DataFrame
+# print("Printing children records:")
+# print(myfamily.loc[["Son","Daughter"]])  #prints the first 2 records in a DataFrame
+
+df = pd.read_csv('employees.txt')
+print(df)
